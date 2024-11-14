0b1e8ecd607cd12b357130be21711c5e54513bca
achdaisy <oduordaizy@gmail.com>
2024-11-14 06:01:20 +0300

    Done but needs some debugging

---
+++ week1/index.py
@@ -0,0 +1,24 @@
+
+first = int(input("Enter the fisrt number: "))
+
+second = int(input("Enter the Second number: "))
+
+operation = input("Enter the mathematical Operation e.g. + , - , /, * ")
+
+if (operation == '+'):
+    result = first + second
+    print(result)
+
+if (operation == '*'):
+    result = first * second
+    print(result)
+
+if (operation == '-'):
+    result = first - second
+    print(result)
+
+if (operation == '/'):
+    result = first / second
+    print(result)
+
+
