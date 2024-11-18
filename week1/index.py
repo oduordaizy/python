6c2d3e5fd5da774637d9714674a58887370aade5
Daisy Oduor <92114263+oduordaizy@users.noreply.github.com>
2024-11-18 14:24:11 +0300

    Handled division by zero

--- week1/index.py
+++ week1/index.py
@@ -1,5 +1,5 @@
 
-first = int(input("Enter the fisrt number: "))
+first = int(input("Enter the first number: "))
 
 second = int(input("Enter the Second number: "))
 
@@ -9,16 +9,25 @@ if (operation == '+'):
     result = first + second
     print(result)
 
-if (operation == '*'):
+elif (operation == '*'):
     result = first * second
     print(result)
 
-if (operation == '-'):
+elif (operation == '-'):
     result = first - second
     print(result)
 
-if (operation == '/'):
-    result = first / second
-    print(result)
+elif (operation == '/'):
+    if (second != 0):
+        result = first / second
+        print(result)
+    else:
+        print("Cannot divide by zero")
+        
+else:
+    print("invalid Operation")
+    
+
+
 
 
